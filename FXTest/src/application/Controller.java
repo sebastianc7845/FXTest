f1e31c45305d1d0065649e2f3cc7b6e436432898
Sebastian Castro <sebastianc7845@gmail.com>
2022-05-19 13:10:17 -0400

    CSS styling

--- FXTest/src/application/Controller.java
+++ FXTest/src/application/Controller.java
@@ -1,17 +1,22 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class Controller {
+	@FXML
+	private Label headingLabel;
+	@FXML
+	private TextField field;
 
 	
 	public void printText(KeyEvent e) {
-		if (e.getCode() == KeyCode.ENTER ) {
-			TextField source = (TextField)e.getSource();
-			System.out.println(source.getText());
-			source.clear();
+		if (e.getCode() == KeyCode.ENTER && e.getSource().equals(field)) {
+			System.out.println(field.getText());
+			field.clear();
 		}
 	}
 }
--- FXTest/src/application/Main.java
+++ FXTest/src/application/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			
 			Scene scene;
 			
 			scene = new Scene(root);
